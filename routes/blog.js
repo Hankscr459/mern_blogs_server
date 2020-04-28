88abe50b862412279703c43379e29a67d8d926b4
xrudoga <defcw58@gmail.com>
2020-04-28 17:47:13 +0800

    Blog- list,listAllBlogsCategoriesTags,read,remove,update,photo

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { create } = require('../controllers/blog')
+const { create, list, listAllBlogsCategoriesTags, read, remove, update, photo } = require('../controllers/blog')
 
 const { requireSignin, authMiddleware } = require('../controllers/auth')
 
 router.post('/blog', requireSignin, authMiddleware, create)
+router.get('/blogs', list)
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags)
+router.get('/blog/:slug', read)
+router.delete('/blog/:slug', requireSignin, authMiddleware, remove)
+router.put('/blog/:slug', requireSignin, authMiddleware, update)
+router.get('/blog/photo/:slug', photo)
 
 module.exports = router
