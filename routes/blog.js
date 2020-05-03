51f75696378fe8fe83dc1edd006132cb29004595
xrudoga <defcw58@gmail.com>
2020-05-03 10:45:39 +0800

    Creating Blogs by Authrnticated User

--- routes/blog.js
+++ routes/blog.js
@@ -9,19 +9,31 @@ const {
     update, 
     photo, 
     listRelated,
-    listSearch
+    listSearch,
+    listByUser
 } = require('../controllers/blog')
 
-const { requireSignin, authMiddleware } = require('../controllers/auth')
+const { 
+    requireSignin, 
+    adminMiddleware, 
+    authMiddleware,
+    canUpdateDeleteBlog
+} = require('../controllers/auth')
 
-router.post('/blog', requireSignin, authMiddleware, create)
+router.post('/blog', requireSignin, adminMiddleware, create)
 router.get('/blogs', list)
 router.post('/blogs-categories-tags', listAllBlogsCategoriesTags)
 router.get('/blog/:slug', read)
-router.delete('/blog/:slug', requireSignin, authMiddleware, remove)
-router.put('/blog/:slug', requireSignin, authMiddleware, update)
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove)
+router.put('/blog/:slug', requireSignin, adminMiddleware, update)
 router.get('/blog/photo/:slug', photo)
 router.post('/blogs/related', listRelated)
 router.get('/blogs/search', listSearch)
+
+// auth user blog crud
+router.post('/user/blog', requireSignin, authMiddleware, create)
+router.get('/:username/blogs', listByUser)
+router.delete('/user/blog/:slug', requireSignin, authMiddleware, canUpdateDeleteBlog, remove)
+router.put('/user/blog/:slug', requireSignin, authMiddleware, canUpdateDeleteBlog, update)
 
 module.exports = router
